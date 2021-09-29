fb515679bbafd98ee48bfdeab92370ea9ae08c6c
RiusmaX <cacahouette72@gmail.com>
2021-09-29 16:23:54 +0200

    add ref to suppress draggable warning

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import Draggable from 'react-draggable'
 import { useCart } from '../contexts/CartContext'
 import CartItem from './CartItem'
@@ -10,10 +11,11 @@ import './styles/Cart.css'
  */
 const Cart = () => {
   const { state: { cart } } = useCart()
+  const nodeRef = useRef(null)
   /* Utilisation de React Draggable pour rendre le composant Cart déplaçable à la souris */
   return (
-    <Draggable>
-      <div className='cart-container'>
+    <Draggable nodeRef={nodeRef}>
+      <div ref={nodeRef} className='cart-container'>
         <h2>Votre commande : </h2>
         {
           // Boucle sur les items du cart pour affichage
