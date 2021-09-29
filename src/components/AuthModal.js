910eea904b3b7ad414ec1e9e1d77b33942a78462
RiusmaX <cacahouette72@gmail.com>
2021-09-29 16:31:21 +0200

    add login form

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -2,12 +2,10 @@ import { useState } from "react"
 import ReactModal from "react-modal"
 import { useAuth, loginUser, registerUser } from "../contexts/AuthContext"
 import { useModal, closeModal } from "../contexts/ModalContext"
+import LoginForm from "./Form/LoginForm"
 import RegisterForm from "./Form/RegisterForm"
 
 function AuthModal () {
-  // Initialisation de l'état local permettant de stocker la saisie de l'utilisateur
-  const [formData, setFormData] = useState({identifier: 'marius@myidea.fr', username: '', email: '', password: '12345678'})
-  
   // Initialisation de l'état local permettant la bascule entre le formulaire d'inscription et de connexion
   const [isRegister, setIsRegister] = useState(false)
 
@@ -17,22 +15,10 @@ function AuthModal () {
   // Récupération de l'état global et de la fonction dispatch en provenance du contexte
   // Renommer le dispatch car déjà importé depuis un autre contexte au dessus
   const { state: { loading }, dispatch: authDispatch} = useAuth()
-  
   // Equivalents de syntaxe
   // const state = useAuth().state
   // const loading = state.loading
   // const dispatch = useAuth().dispatch
-
-  /**
-   * Gère la saisie de l'utilisateur
-   * @param {*} e évènement provenant du HTML (correspondant à l'action de frappe sur le clavier)
-   */
-  const handleChange = (e) => {
-    setFormData({
-      ...formData, // Fusion avec les données déjà présentes dans l'état local
-      [e.target.name]: e.target.value // Utilisation du name pour remplir l'état local dynamiquement
-    })
-  }
 
   /**
    * Gère le clic sur le bouton d'inscription
@@ -47,8 +33,7 @@ function AuthModal () {
    * Gère le clic sur le bouton de connexion
    * @param {*} e évènement provenant du HTML (correspondant à l'action du clic sur le bouton)
    */
-  const handleSubmitLogin = async (e) => {
-    e.preventDefault()
+  const handleSubmitLogin = async (formData) => {
     loginUser(authDispatch, formData)
     closeModal(dispatch)
   }
@@ -70,21 +55,7 @@ function AuthModal () {
             <RegisterForm onSubmit={handleSubmitRegister} loading={loading} />
           )
           : (
-            <div>
-              <form onSubmit={handleSubmitLogin}>
-                <label>
-                  Email :
-                  <input name='identifier' onChange={handleChange} value={formData.identifier} />
-                </label>
-                <br />
-                <label>
-                  Password :
-                  <input type='password' name='password' onChange={handleChange} value={formData.password} />
-                </label>
-                <br />
-                <input type='submit' value={loading ? "Chargement..." : "Se connecter"} />
-              </form>
-            </div>
+            <LoginForm onSubmit={handleSubmitLogin} loading={loading} />
           )
         }
         <button onClick={handleToggleRegisterMode}>
---
+++ src/components/Form/LoginForm.js
@@ -0,0 +1,63 @@
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+import TextInput from "./TextInput"
+
+// Définition des propriétés
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  loading: PropTypes.bool
+}
+
+/**
+ * Composant contenant le formulaire d'enregistrement
+ * @param {*} props Propriétés du composant
+ * @returns Un Composant
+ */
+ function LoginForm ({ onSubmit, loading, ...props }) {
+  // Initialisation de l'état local permettant de stocker la saisie de l'utilisateur
+  const [formData, setFormData] = useState({identifier: '', password: ''})
+  
+  /**
+   * Gère la saisie de l'utilisateur
+   * @param {*} e évènement provenant du HTML (correspondant à l'action de frappe sur le clavier)
+   */
+  const handleChange = (e) => {
+    setFormData({
+      ...formData, // Fusion avec les données déjà présentes dans l'état local
+      [e.target.name]: e.target.value // Utilisation du name pour remplir l'état local dynamiquement
+    })
+  }
+
+  /**
+   * Gère le clic sur le bouton d'inscription
+   * @param {*} e évènement provenant du HTML (correspondant à l'action du clic sur le bouton)
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onSubmit(formData)
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextInput
+        label='Email'
+        name='identifier'
+        onChange={handleChange}
+        value={formData.identifier} 
+      />
+      <br />
+      <TextInput
+        type='password'
+        label='Password'
+        name='password'
+        onChange={handleChange}
+        value={formData.password} 
+      />
+      <br />
+      <input type='submit' value={loading ? "Chargement..." : "Se connecter"} />
+    </form>
+  )
+}
+
+export default LoginForm
