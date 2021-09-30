8b7d17fd5cf9d2737076bdd2c236230c7723209a
RiusmaX <cacahouette72@gmail.com>
2021-09-30 16:52:42 +0200

    start styling menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,22 @@
-import { Button } from '@mui/material'
+import { Button, Grid } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 function Menu () {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to='/' style={{ textDecoration: 'none' }}>
-            <Button variant='contained'>Accueil</Button>
-          </Link>
-        </li>
-        <li>
-          <Link to='/restaurants' style={{ textDecoration: 'none' }}>
-            <Button variant='contained'>Restaurants</Button>
-          </Link>
-        </li>
-        <li>
-          <Link to='/components' style={{ textDecoration: 'none' }}>
-            <Button variant='contained'>Components</Button>
-          </Link>
-        </li>
-      </ul>
+      <Grid container spacing={2}>
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          <Button variant='contained'>Accueil</Button>
+        </Link>
+
+        <Link to='/restaurants' style={{ textDecoration: 'none' }}>
+          <Button variant='contained'>Restaurants</Button>
+        </Link>
+
+        <Link to='/components' style={{ textDecoration: 'none' }}>
+          <Button variant='contained'>Components</Button>
+        </Link>
+      </Grid>
     </nav>
   )
 }
